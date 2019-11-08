de8f1580a8711340d7037624ee7f93c281edb424
Jaime Gomes <jaime.gomes@calipo.io>
2019-11-08 09:56:44 +0100

    Implement both create and close actions

--- lib/exgithub/controller.ex
+++ lib/exgithub/controller.ex
@@ -9,9 +9,42 @@ defmodule ExGitHub.Controller do
   @authorization_token System.get_env("JIRA_AUTH_TOKEN")
 
   def create(request) do
-    payload = ExGitHub.Parser.parse_jira(request)
     {:ok, client} = Gira.new(@base_url, @authorization_token)
-    {status, response} = Gira.create_issue_with_basic_info(client, payload)
-    response
+
+    with  {:ok, jira_resp} <- search_github_on_jira(client, request["issue"]["number"]),
+          false <- is_exist(jira_resp),
+          parse = ExGitHub.Parser.parse_jira(request) do
+      Logger.debug("jira issue with id #{jira_id} will be closed")
+      {_status, response} = Gira.create_issue_with_basic_info(client, parse)
+      response
+    else
+      true -> %{status: 200, payload: %{msg: "github issue already exists in jira"}}
+    end
+  end
+
+  def close(request) do
+    {:ok, client} = Gira.new(@base_url, @authorization_token)
+
+    with  {:ok, jira_resp} <- search_github_on_jira(client, request["issue"]["number"]),
+          true <- is_exist(jira_resp),
+          jira_id = Enum.at(jira_resp.payload["issues"],0)["id"] do
+        Logger.debug("jira issue with id #{jira_id} will be closed")
+        {_status, response} = Gira.close_issue(client, %{ jira_id: jira_id, transition_id: "31" })
+        response
+    else
+      true -> %{status: 200, payload: %{msg: "github issue already exists in jira"}}
+    end
+  end
+
+  defp search_github_on_jira(client, github_id) do
+    filter = "labels%3DGitHub-#{github_id}"
+
+    Logger.debug("check if github id #{github_id} exists in jira using filter #{filter}")
+
+    Gira.get_issue_basic_info_by_query(client, filter)
+  end
+
+  defp is_exist(resp) do
+    resp.status == 200
   end
 end
--- lib/exgithub/endpoint.ex
+++ lib/exgithub/endpoint.ex
@@ -8,27 +8,42 @@ defmodule ExGitHub.Endpoint do
   alias ExGitHub.Plug.{SignatureVerification, CacheBodyReader}
 
   @secret_token Application.get_env(:exgithub, :secret_token)
+  @label "SUSE"
 
-  plug :match
-  plug Plug.Logger
-  plug Plug.RequestId
-  plug Plug.Parsers,
+  plug(:match)
+  plug(Plug.Logger)
+  plug(Plug.RequestId)
+
+  plug(Plug.Parsers,
     parsers: [:json],
     body_reader: {CacheBodyReader, :read_body, []},
     json_decoder: Jason
-  plug SignatureVerification, header: "x-hub-signature", secret: @secret_token
-  plug :dispatch
+  )
+
+  plug(SignatureVerification, header: "x-hub-signature", secret: @secret_token)
+  plug(:dispatch)
 
   get "/health" do
     send_resp(conn, 200, "OK")
   end
 
   post "/events" do
-    {:ok, resp} = process_request(conn.body_params)
-    resp
-    |> send_response(conn)
-  end
+    # Check if it contains the 'SUSE' label
+    with true <- is_label_present(conn.body_params["issue"]["labels"], @label) do
+      {:ok, resp} = process_request(conn.body_params)
 
+      resp
+      |> send_response(conn)
+    else
+      false ->
+        Logger.info("discarding the github issue due lack of #{@label} label")
+
+        send_response(
+          %{status: 200, payload: "discarding the github issue due lack of trigger label(s)"},
+          conn
+        )
+    end
+  end
 
   match _ do
     send_resp(conn, 404, "oops... Nothing here :(")
@@ -42,7 +57,7 @@ defmodule ExGitHub.Endpoint do
 
   # called when a Github issue is created.
   defp process_request(payload = %{"action" => "opened"}) do
-    Logger.debug("processing request from github...")
+    Logger.debug("creating a new github issue...")
     response = ExGitHub.Controller.create(payload)
 
     {:ok,
@@ -54,12 +69,16 @@ defmodule ExGitHub.Endpoint do
   end
 
   # called when a Github issue is closed.
-  defp process_request(%{"action" => "closed"}) do
+  defp process_request(payload = %{"action" => "closed"}) do
+    Logger.debug("closing a github issue...")
+    response = ExGitHub.Controller.close(payload)
+
     {:ok,
      %{
-       status: 200,
+       status: response.status,
        payload: %{
          action: "closed"
+         payload: response.payload
        }
      }}
   end
@@ -94,6 +113,11 @@ defmodule ExGitHub.Endpoint do
        }
      }}
   end
+
+  defp is_label_present(labels, label_to_find) when not is_nil(labels),
+    do: Enum.member?(labels, label_to_find)
+
+  defp is_label_present(labels, label_to_find) when is_nil(labels), do: false
 
   # def child_spec(opts) do
   #  %{
