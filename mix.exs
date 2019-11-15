a07123dcbcfa82016c75d264716952e869d6b45b
Jaime Gomes <jaime.gomes@protonmail.com>
2019-11-15 16:36:00 +0100

    Add mock library Mox

--- mix.exs
+++ mix.exs
@@ -29,8 +29,7 @@ defmodule ExGitHub.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:gira, "~> 0.3.0", override: true},
       {:excoveralls, "~> 0.7", only: :test},
-      # {:meck, "~> 0.8.9", only: [:dev, :test]},
-      # {:mock, "~> 0.2", only: [:dev, :test]},
+      {:mox, "~> 0.5.1", only: [:dev, :test]},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:inch_ex, "~>2.0", only: [:dev, :test]},
       {:benchfella, "~> 0.3.3", only: :dev}
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,7 @@
 ExUnit.start()
 
+Mox.defmock(ExGitHub.GiraApiMock, for: ExGitHub.GiraApi)
+
 defmodule ExGitHub.Helpers do
   def generate_http_signature(secret, payload) do
     :crypto.hmac(:sha, secret, payload) |> Base.encode16(case: :lower)
