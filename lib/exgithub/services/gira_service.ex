d6887909b518e4d54a54f612c72ccaa41bf1f09e
Jaime Gomes <jaime.gomes@protonmail.com>
2019-11-18 15:35:56 +0100

    another try

--- lib/exgithub/services/gira_service.ex
+++ lib/exgithub/services/gira_service.ex
@@ -49,7 +49,8 @@ defmodule ExGitHub.Services.GiraService do
   @impl GiraServiceProvider
   def get(filter) do
     {:ok, client} = Gira.new(@base_url, @authorization_token)
-    #{_status, response} = Gira.get_issue_basic_info_by_query(client, filter)
+    Gira.get_issue_basic_info_by_query(client, filter)
+    |> IO.inspect
     Logger.info("returning jira search response")
     %{status: 500, payload: "error"}
   end
