b61bf8b48ffed999f37f1235783781c726d48a19
Jaime Gomes <jaime.gomes@protonmail.com>
2019-11-18 16:39:31 +0100

    replace fetch with get env vars

--- config/releases.exs
+++ config/releases.exs
@@ -1,10 +1,10 @@
 import Config
 
 config :exgithub,
-  port: String.to_integer(System.fetch_env!("PORT")),
-  secret_token: System.fetch_env!("SECRET_TOKEN"),
-  jira_base_url: System.fetch_env!("JIRA_BASE_URL"),
-  jira_auth_token: System.fetch_env!("JIRA_AUTH_TOKEN"),
+  port: String.to_integer(System.get_env("PORT")),
+  secret_token: System.get_env("SECRET_TOKEN"),
+  jira_base_url: System.get_env("JIRA_BASE_URL"),
+  jira_auth_token: System.get_env("JIRA_AUTH_TOKEN"),
   github_trigger_label: "SUSE"
 
 config :exgithub, port: 8080
