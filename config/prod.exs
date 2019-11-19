6cba3fb1412b1585aa59bb37baaaf44a6926a6d1
Jaime Gomes <jaime.gomes@calipo.io>
2019-11-19 09:26:29 +0100

    Remove some gigalixir settings

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,6 @@
 use Mix.Config
 
 config :exgithub,
-  server: true,
   secret_token: System.get_env("SECRET_TOKEN"),
   jira_base_url: System.get_env("JIRA_BASE_URL"),
   jira_auth_token: System.get_env("JIRA_AUTH_TOKEN"),
