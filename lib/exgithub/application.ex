9e193bc5b8d795fda87ccd6b10da96b648ca1f82
Jaime Gomes <jaime.gomes@protonmail.com>
2019-11-21 17:02:01 +0100

    Fix port names in different places

--- lib/exgithub/application.ex
+++ lib/exgithub/application.ex
@@ -9,8 +9,8 @@ defmodule ExGitHub.Application do
 
     port =
       get_port(
-        Application.get_env(:exgithub, :port),
-        String.length(Application.get_env(:exgithub, :port))
+        Application.get_env(:exgithub, :http_port),
+        String.length(Application.get_env(:exgithub, :http_port))
       )
 
     children = [
@@ -31,15 +31,15 @@ defmodule ExGitHub.Application do
   defp build_app_env() do
     Logger.info("setting app variables")
 
-    port = System.get_env("HTTP_PORT")
+    http_port = System.get_env("HTTP_PORT")
     secret_token = System.get_env("SECRET_TOKEN")
     jira_base_url = System.get_env("JIRA_BASE_URL")
     jira_auth_token = System.get_env("JIRA_AUTH_TOKEN")
     label = System.get_env("LABEL")
 
-    Logger.info("Label: #{label}, URL: #{jira_base_url}:#{port}")
+    Logger.info("Label: #{label}, URL: #{jira_base_url}:#{http_port}")
 
-    Application.put_env(:exgithub, :port, port)
+    Application.put_env(:exgithub, :http_port, http_port)
     Application.put_env(:exgithub, :secret_token, secret_token)
     Application.put_env(:exgithub, :jira_base_url, jira_base_url)
     Application.put_env(:exgithub, :jira_auth_token, jira_auth_token)
@@ -47,5 +47,5 @@ defmodule ExGitHub.Application do
   end
 
   defp get_port(port, length) when not is_nil(port) and length > 0, do: String.to_integer(port)
-  defp get_port(_, _), do: 80
+  defp get_port(_, _), do: 4000
 end
