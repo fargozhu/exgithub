200b506b18028f2959e943da832f7ecc57d2383f
Jaime Gomes <jaime.gomes@protonmail.com>
2019-11-28 09:50:36 +0100

    Make all debug

--- config/config.exs
+++ config/config.exs
@@ -5,4 +5,6 @@ import_config "#{Mix.env()}.exs"
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+  metadata: [:request_id],
+  level: :debug
+
